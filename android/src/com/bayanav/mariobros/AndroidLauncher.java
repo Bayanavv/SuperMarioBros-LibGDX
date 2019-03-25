1ac16705df5cab6416cbed529bfda4b79a6dc02b
sergen <bayanav.sergen@icloud.com>
2019-03-25 17:56:19 +0200

    Super Mario Bros - LibGDX : the beginning

---
+++ android/src/com/bayanav/mariobros/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bayanav.mariobros;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bayanav.mariobros.marioBros;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new marioBros(), config);
+	}
+}
---
+++ desktop/src/com/bayanav/mariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bayanav.mariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bayanav.mariobros.marioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new marioBros(), config);
+	}
+}
