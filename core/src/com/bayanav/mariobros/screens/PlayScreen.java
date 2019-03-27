3540111506c3342b143f8075e8a6ff8ad1e171be
sergen <bayanav.sergen@icloud.com>
2019-03-27 23:36:53 +0200

    Super Mario Bros - LibGDX : Game Camera

--- core/src/com/bayanav/mariobros/screens/PlayScreen.java
+++ core/src/com/bayanav/mariobros/screens/PlayScreen.java
@@ -4,18 +4,25 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.bayanav.mariobros.MarioBros;
-
-
 
 public class PlayScreen implements Screen {
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(800,480,gameCam);
+
 
     }
 
@@ -28,6 +35,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
         game.batch.begin();
         game.batch.draw(texture,0,0);
         game.batch.end();
@@ -36,6 +44,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
+        gamePort.update(width,height);
 
     }
 
