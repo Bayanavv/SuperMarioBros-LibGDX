a5808a249902ca7348ae12940a947deafa5de1bd
sergen <bayanav.sergen@icloud.com>
2019-03-31 19:50:28 +0300

    Super Mario Bros - LibGDX : Box2D

--- core/src/com/bayanav/mariobros/screens/PlayScreen.java
+++ core/src/com/bayanav/mariobros/screens/PlayScreen.java
@@ -6,9 +6,19 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -22,23 +32,101 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hub hub;
 
+    //Tiled map variables
     private TmxMapLoader maploader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
+    //Box2D variables
+    private World world;
+    private Box2DDebugRenderer b2dr;//gives us a graphical representation of our fixtures and bodies inside of our box2d world
+
     public PlayScreen(MarioBros game) {
         this.game = game;
+
         //create cam used to follow mario through cam world
         gameCam = new OrthographicCamera();
+
         //create FitViewport to maintain virtual aspect ratio despite
         gamePort = new FitViewport(GameManager.V_WIDTH,GameManager.V_HEIGHT,gameCam);
+
         //create our game HUB for scores/timers/level info
         hub = new Hub(game.batch);
+
         //lead our map and setup our map renderer
         maploader = new TmxMapLoader();
         map = maploader.load("maps/level1.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
+
+        //initially set our gamCam to be centered correctly at the start of of
         gameCam.position.set(gamePort.getWorldWidth()/2,gamePort.getWorldHeight()/2,0);
+
+        world = new World(new Vector2(0,0), true);//no gravity for now we change it in the feature, second parameter is do we want to sleep object that are at rest
+        //box2d doesn't calculate inside its physics simulation bodies they are at rest so is save some time when it's doing those calculations , you can always wake up an object by commanding it to do any activity
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();//need have before you can create a fixture likewise you need to define that fixture first and then add it to the body
+        Body body;
+
+        //create ground bodies/ fixtures
+        for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2,rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        for (MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2,rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        for (MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2,rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        for (MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2,rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
 
     }
 
@@ -59,14 +147,22 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+
+        //separate our update logic from render
         update(delta);
+
+
         renderer.setView(gameCam);
 
         //clear the game screen with black
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //render our game map
         renderer.render();
+
+        //renderer our box2DDebugLines
+        b2dr.render(world, gameCam.combined);
 
         //Set our batch to now draw what HUb camera sees.
         game.batch.setProjectionMatrix(hub.stage.getCamera().combined );//what gonna be shown via our camera
