0bb57927d07db2802380b67444fc53c9ea0f8ab2
sergen <bayanav.sergen@icloud.com>
2019-04-04 12:23:02 +0300

    Super Mario Bros - LibGDX : Creating some extra Classes to code cleaning

--- core/src/com/bayanav/mariobros/actors/effects/Effect.java
+++ core/src/com/bayanav/mariobros/actors/effects/Effect.java
@@ -1,4 +1,16 @@
 package com.bayanav.mariobros.actors.effects;
 
-public class Effect {
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.bayanav.mariobros.actors.RigidBody;
+import com.bayanav.mariobros.screens.PlayScreen;
+
+public abstract class Effect extends RigidBody {
+
+    protected TextureAtlas textureAtlas;
+
+    public Effect(PlayScreen playScreen, float x, float y) {
+        super(playScreen, x, y);
+        this.textureAtlas = playScreen.getTextureAtlas();
+
+    }
 }
