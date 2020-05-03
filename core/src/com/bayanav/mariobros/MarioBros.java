df81e31d7db9bd7c29d74fd2b504952bf8024e03
sergen <bayanav.sergen@icloud.com>
2020-05-03 14:13:49 +0300

    Update MarioBros.java

--- core/src/com/bayanav/mariobros/MarioBros.java
+++ core/src/com/bayanav/mariobros/MarioBros.java
@@ -6,11 +6,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.bayanav.mariobros.manager.GameManager;
 import com.bayanav.mariobros.screens.PlayScreen;
 
 public class MarioBros extends Game {
-
 	/**
 	 spriteBatch is basically a container that holds all of our images or textures and stuff like
 	that in when we are ready to render them to the screen the sprite batch basically spills them
@@ -20,20 +18,12 @@ public class MarioBros extends Game {
 	 to be creating new spriteBatches all the time
 	 */
 	public SpriteBatch batch;
-
-
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-//		if (GameManager.instance != null) {
-//			gameManager = GameManager.instance;
-//		}
-//		else {
-//			gameManager = new GameManager();
-//		}
-		//setScreen(new PlayScreen(this));//passing the game it self
-		setScreen(new PlayScreen(this));
+		setScreen(new PlayScreen(this));//passing the game it self
+
 	}
 
 
